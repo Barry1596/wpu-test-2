9bbb557a5e61deb9e59f0414ec162c71f763ebbe
Barry1596 <fbarryalfian@gmail.com>
2021-06-24 09:12:55 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Coba Git</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam error voluptatum eum maiores omnis id. Voluptas voluptate commodi ullam quidem ea, magnam repudiandae animi tempora, cum maiores iusto sed cupiditate.</p>
+    </body>
+</html>
